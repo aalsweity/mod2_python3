879e3978d471f666433d3ab4dcac38a7a8b339f9
Anas Al-Sweity <aalsweity@gmail.com>
2024-02-03 13:02:15 -0500

    first commit

---
+++ module.py
@@ -0,0 +1,5 @@
+def area():
+    length = int(input("What is the length of your house?: "))
+    width = int(input("What is the width of your house?: "))
+    t_area = length * width
+    return t_area
