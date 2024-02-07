bcf2b18bdf23f7a4b186a14e31cac6824503668d
Anas Al-Sweity <aalsweity@gmail.com>
2024-02-07 13:14:35 -0500

    second commit

--- module.py
+++ module.py
@@ -1,5 +1,11 @@
 def area():
-    length = int(input("What is the length of your house?: "))
-    width = int(input("What is the width of your house?: "))
+    length = float(input("What is the length of your house?: "))
+    width = float(input("What is the width of your house?: "))
     t_area = length * width
     return t_area
+
+def circumference():
+    pi = 3.14
+    radius = float(input("What is the radius of the circle?: "))
+    c_circ = 2*pi*radius
+    return c_circ
